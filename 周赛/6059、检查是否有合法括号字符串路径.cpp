b23b35e87ab04fac6ca41e4ff9cf3c23056a6780
whdcwsj <maxinaerichared@gmail.com>
2022-05-09 11:07:28 +0800

    0508补充

--- 周赛/6059、检查是否有合法括号字符串路径.cpp
+++ 周赛/6059、检查是否有合法括号字符串路径.cpp
@@ -2,31 +2,47 @@
 using namespace std;
 
 
+//难点在于，到每个位置的时候，可能有2个得分信息,即是否可能的选项，因为有两条路径
+
 //以dp数组来记录到每一步是否合法
 //左括号+1，右括号-1
 //记f(i,j,k)表示是否存在以格子(i,j)为结尾，且now值是k的括号序列
 //维护一个变量k记录数值
 bool hasValidPath(vector<vector<char>>& grid) {
-        if (grid[0][0] == ')') return false;
-
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<vector<bool>>> f;
-        for (int i = 0; i < n; i++) {
-            f.push_back(vector<vector<bool>>());
-            for (int j = 0; j < m; j++) f.back().push_back(vector<bool>(n + m));
-        }
-        f[0][0][1] = true;
-        for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) if (i || j) {
-            int t = (grid[i][j] == '(' ? 1 : -1);
-            for (int k = 0; k < n + m; k++) {
-                int kk = k - t;
-                if (kk < 0 || kk >= n + m) continue;
-                if (i) f[i][j][k] = f[i][j][k] || f[i - 1][j][kk];
-                if (j) f[i][j][k] = f[i][j][k] || f[i][j - 1][kk];
+        if(grid[0][0]==')') return false;
+        int n=grid.size();
+        int m=grid[0].size();
+        //初始化，三维dp
+        vector<vector<vector<bool>>>dp;
+        for(int i=0;i<n;i++){
+            dp.push_back(vector<vector<bool>>());
+            for(int j=0;j<m;j++){
+                dp.back().push_back(vector<bool>(n+m));
             }
         }
-        return f[n - 1][m - 1][0];
-}
+        dp[0][0][1]=true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                //排除[0,0]
+                if(i!=0 || j!=0){
+                    int temp=(grid[i][j]=='('?1:-1);
+                    for(int k=0;k<n+m;k++){
+                        //上一步的得分
+                        int kk=k-temp;
+                        if(kk<0 || kk>=n+m) continue;
+                        //两个方向的行动路线
+                        if(i>=1){
+                            dp[i][j][k]=(dp[i][j][k] || dp[i-1][j][kk]);
+                        }
+                        if(j>=1){
+                            dp[i][j][k]=(dp[i][j][k] || dp[i][j-1][kk]);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[n-1][m-1][0];
+    }
 
 
 int main(){
